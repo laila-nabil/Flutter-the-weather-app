f80d7cde097be76052dd8bcdff3dbaccfa314662
laila.nabil <laila.n.mostafa@gmail.com>
2021-12-25 00:43:53 +0200

    tabs instead of list trial

---
+++ lib/models/weather_tabs.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class WeatherTabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            DefaultTabController(
+                length: 8, // length of tabs
+                initialIndex: 0,
+                child: Column(
+                    crossAxisAlignment:
+                    CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Container(
+                        child: TabBar(
+                          isScrollable: true,
+                          labelColor: Colors.green,
+                          unselectedLabelColor: Colors.black,
+                          tabs: [
+                            Tab(text: 'Tab 1'),
+                            Tab(text: 'Tab 2'),
+                            Tab(text: 'Tab 3'),
+                            Tab(text: 'Tab 4'),
+                            Tab(text: 'Tab 1'),
+                            Tab(text: 'Tab 2'),
+                            Tab(text: 'Tab 3'),
+                            Tab(text: 'Tab 4'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                          height: 100, //height of TabBarView
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  top: BorderSide(
+                                      color: Colors.grey,
+                                      width: 0.5))),
+                          child:
+                          TabBarView(children: <Widget>[
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 1',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 2',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 3',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 4',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 1',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 2',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 3',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 4',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight:
+                                        FontWeight.bold)),
+                              ),
+                            ),
+                          ]))
+                    ])),
+          ]),
+    );
+  }
+}
