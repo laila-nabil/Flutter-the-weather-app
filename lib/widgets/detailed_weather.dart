67a2639b1b0a77f2220888072c01b77fa02226d8
laila.nabil <laila.n.mostafa@gmail.com>
2022-01-03 16:29:59 +0200

    fix ui

--- lib/widgets/detailed_weather.dart
+++ lib/widgets/detailed_weather.dart
@@ -14,67 +14,73 @@ class WeatherDetailed extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(topRight: Radius.circular(30),topLeft: Radius.circular(30)),
-        color: Theme.of(context).backgroundColor,
+    final screenSize = MediaQuery.of(context).size;
+    return LayoutBuilder(builder: (ctx , constraints){
+      print("for detailed,$constraints");
+      return Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(topRight: Radius.circular(30),topLeft: Radius.circular(30)),
+          color: Theme.of(context).backgroundColor,
 
-      ),
-      // height: 300,
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          weatherDay.isImageNetwork
-              ? Image.network(
-            weatherDay.image,
-            width: MediaQuery.of(context).size.width * 0.7,
-            fit: BoxFit.fitWidth,
-          )
-              : Image.asset(
-            weatherDay.image,
-            width: MediaQuery.of(context).size.width * 0.7,
-            fit: BoxFit.fitWidth,
-            // width: 150,
-            // height: 150,
-          ),
-          Text(
-            '${weatherDay.tempCurrent} °C',
-            style: TextStyle(fontSize: 25),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if(weatherDay.feelsLike!=null && weatherDay.feelsLike!="")
-                Text(
-                'Feels like ${weatherDay.feelsLike}°C, ',
-                style:
-                TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-              Text(weatherDay.detailedDescription),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              if(weatherDay.rain!=null && weatherDay.rain!="")
-              dashboardWeather(
-                svgIcon: 'assets/dashboard_icons/rain.svg',
-                status: '${double.parse(weatherDay.rain) * 100.0}%',
-              ),
-              if(weatherDay.windSpeed!=null && weatherDay.windSpeed!="")
-              dashboardWeather(
-                svgIcon: 'assets/dashboard_icons/wind_2.svg',
-                status:
+        ),
+        // height: 300,
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            weatherDay.isImageNetwork
+                ? Image.network(
+              weatherDay.image,
+              width: constraints.maxWidth * 0.8,
+              height: constraints.maxHeight * 0.4,
+              fit: BoxFit.contain,
+            )
+                : Image.asset(
+              weatherDay.image,
+              width: constraints.maxWidth * 0.8,
+              height: constraints.maxHeight * 0.4,
+              fit: BoxFit.contain,
+              // width: 150,
+              // height: 150,
+            ),
+            Text(
+              '${weatherDay.tempCurrent} °C',
+              style: TextStyle(fontSize: 25),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if(weatherDay.feelsLike!=null && weatherDay.feelsLike!="")
+                  Text(
+                    'Feels like ${weatherDay.feelsLike}°C, ',
+                    style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                Text(weatherDay.detailedDescription),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                if(weatherDay.rain!=null && weatherDay.rain!="")
+                  dashboardWeather(
+                    svgIcon: 'assets/dashboard_icons/rain.svg',
+                    status: '${double.parse(weatherDay.rain) * 100.0}%',
+                  ),
+                if(weatherDay.windSpeed!=null && weatherDay.windSpeed!="")
+                  dashboardWeather(
+                    svgIcon: 'assets/dashboard_icons/wind_2.svg',
+                    status:
                     '${weatherDay.windSpeed} m/s ${windDirection(int.parse(weatherDay.windDeg))}',
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+                  ),
+              ],
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
 
