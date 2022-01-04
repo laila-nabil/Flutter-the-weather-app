6583118fdecca6cf026398183ee49b068e843eff
laila.nabil <laila.n.mostafa@gmail.com>
2022-01-04 22:56:25 +0200

    expanded may caused error on web

--- lib/widgets/weather_tabs.dart
+++ lib/widgets/weather_tabs.dart
@@ -9,8 +9,7 @@ class WeatherTabs extends StatelessWidget {
   Widget build(BuildContext context) {
     final weatherTabs = Provider.of<WeatherProvider>(context).allWeather;
     print('weatherTabs.length, ${weatherTabs.length}');
-    double tabHeight = 0;
-    print("tabHeight $tabHeight");
+    double tabBarContainerHeight = 0;
     return LayoutBuilder(builder: (ctx,constraints){
       print("weatherTabs constraints $constraints");
       return DefaultTabController(
@@ -26,7 +25,7 @@ class WeatherTabs extends StatelessWidget {
                 unselectedLabelColor: Colors.grey,
                 tabs: [
                   ...weatherTabs.map((e){
-                    final tab = Tab(
+                    final tabBarContainer = Container(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
@@ -78,19 +77,73 @@ class WeatherTabs extends StatelessWidget {
                         ],
                       ),
                     );
+                    final tab = Tab(
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            if (e.isImageNetwork &&
+                                (e.image != null &&
+                                    e.image != "" &&
+                                    e.image !=
+                                        'assets/weather_status/clear.png'))
+                              Image.network(
+                                e.image,
+                                width: 50,
+                                height: 50,
+                              ),
+                            if (!e.isImageNetwork &&
+                                (e.image != null &&
+                                    e.image != "" &&
+                                    e.image !=
+                                        'assets/weather_status/clear.png'))
+                              Image.asset(
+                                e.image,
+                                width: 35,
+                                height: 35,
+                              ),
+                            Column(
+                              children: [
+                                Text(
+                                  DateFormat.MMMEd().format(e.date),
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                RichText(
+                                  text: TextSpan(children: [
+                                    TextSpan(
+                                        text:
+                                        "${double.parse(e.tempMax).toStringAsFixed(1)} ",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white
+                                        )),
+                                    TextSpan(
+                                        text:
+                                        "${double.parse(e.tempMin).toStringAsFixed(1)} ",
+                                        style:
+                                        TextStyle(color: Colors.white)
+                                    ),
+                                  ]),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
                     return tab;
                   })
                 ],
               ),
             ),
             Container(
-                // height: 150, //height of TabBarView
               height: constraints.maxHeight - 49,
                 alignment: Alignment.topRight,
                 decoration: BoxDecoration(
                     border: Border(
                         top: BorderSide(color: Colors.grey, width: 0.5))),
-                child: TabBarView(children: <Widget>[
+                child: TabBarView(
+                    children: <Widget>[
                   ...weatherTabs
                   // .map((e) => WeatherList(e.weatherTimeline))
                       .map((e) {
