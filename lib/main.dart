fc0cae565d2f359c5766099b77041ddef0c31b23
laila.nabil <laila.n.mostafa@gmail.com>
2022-01-17 05:54:59 +0200

    location_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,7 @@ class MyApp extends StatelessWidget {
           textTheme: Typography().white,
         ),
         home: MyHomePage(),
+        // home: LocationScreen(),
         // home: testPage(),
         // home: dummyScreen(),
         routes: {LocationScreen.routeName: (ctx) => LocationScreen()},
@@ -94,3 +95,63 @@ class MyCustomScrollBehavior extends MaterialScrollBehavior {
         PointerDeviceKind.mouse,
       };
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_mapbox_autocomplete/flutter_mapbox_autocomplete.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter MapBox AutoComplete',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: Home(),
+//     );
+//   }
+// }
+//
+// class Home extends StatefulWidget {
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//   final _startPointController = TextEditingController();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Flutter MapBox AutoComplete example"),
+//       ),
+//       body: SingleChildScrollView(
+//         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+//         child: CustomTextField(
+//           hintText: "Select starting point",
+//           textController: _startPointController,
+//           onTap: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => MapBoxAutoCompleteWidget(
+//                   apiKey: 'pk.eyJ1IjoibG45NSIsImEiOiJja3kzaXUwaXIwMmcyMnhvamtoZTA2YnpiIn0.NY30EgaJ_bcBMc-JKqSs1g',
+//                   hint: "Select starting point",
+//                   onSelect: (place) {
+//                     _startPointController.text = place.placeName;
+//                   },
+//                   limit: 10,
+//                   country: "EG",
+//                 ),
+//               ),
+//             );
+//           },
+//           enabled: true,
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -14,8 +14,45 @@ class LocationScreen extends StatelessWidget {
         minimum: EdgeInsets.zero,
         child: Scaffold(
           backgroundColor: Theme.of(context).backgroundColor,
-          body: TextField(
-            style: TextStyle(color: Colors.white),
+          body: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Location',
+                  style: TextStyle(
+                      fontSize: 34,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.symmetric(vertical: 1.0,horizontal: 6.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.white.withOpacity(0.2), width: 1.0),
+                      borderRadius: BorderRadius.circular(15),
+                      color: Color(0xff2d3647)),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        label: Row(
+                          children: [
+                            Icon(Icons.search , color: Colors.white,),
+                            SizedBox(width: 4,),
+                            Text('Add Location'),
+                          ],
+                        ),
+                        labelStyle: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                        fillColor: Colors.purple),
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
