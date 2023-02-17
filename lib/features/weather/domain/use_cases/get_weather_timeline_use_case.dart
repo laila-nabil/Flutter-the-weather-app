97ebe329db3603673ba6707838a247de34bf4296
laila.nabil <laila.nabil.mustafa@gmail.com>
2023-02-17 21:30:26 +0200

    weather_timeline.dart

---
+++ lib/features/weather/domain/use_cases/get_weather_timeline_use_case.dart
@@ -0,0 +1,13 @@
+// import 'package:equatable/equatable.dart';
+// import 'package:the_weather_app/core/use_case/use_case.dart';
+// class GetWeatherTimelineUsecase implements UseCase<,WeatherTimelineParams>{
+//
+// }
+//
+//
+// class WeatherTimelineParams extends Equatable{
+//   @override
+//   // TODO: implement props
+//   List<Object?> get props => throw UnimplementedError();
+//
+// }
